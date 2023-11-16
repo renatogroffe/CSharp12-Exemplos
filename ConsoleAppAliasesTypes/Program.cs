1b6bfb234047a4121a124911a1e19c39144b2c35
Renato Groffe <renato.groffe@yahoo.com.br>
2023-11-15 22:40:34 -0300

    Alias any type
    Alias any type

---
+++ ConsoleAppAliasesTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleAppAliasesTypes;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("***** Testes com C# 12 | Utilizando Aliases para Tipos *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName}- Kernel: {Environment
+    .OSVersion.VersionString}");
+
+Testes.ConverterTemperaturas(new double[] { 86, 32, 212, 71.6, 104 });
+
+Testes.CalcularIMC((70.5, 2.05));
+Testes.CalcularIMC((75.12, 1.81));
+Testes.CalcularIMC((130.0, 1.90));
+Testes.CalcularIMC((150.0, 1.60));
+Testes.CalcularIMC((70.98, 1.54));
---
+++ ConsoleAppAliasesTypes/Testes.cs
@@ -0,0 +1,45 @@
+﻿using ConversoesFahrenheit = double[];
+using CalculoIMC = (double Peso, double Altura);
+
+namespace ConsoleAppAliasesTypes;
+
+public static class Testes
+{
+    public static void ConverterTemperaturas(
+        ConversoesFahrenheit temperaturasFahrenheit)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Testes com o alias 'ConversoesFahrenheit'...");
+        foreach (var tempFahrenheit in temperaturasFahrenheit)
+        {
+            var tempCelsius = Math.Round((tempFahrenheit - 32) / 1.8, 2);
+            var tempKelvin = tempCelsius + 273.15;
+            Console.WriteLine(
+                $"{tempFahrenheit} graus Fahrenheit = {tempCelsius} graus Celsius = " +
+                $"{tempKelvin} graus Kelvin");
+        }
+    }
+
+    public static void CalcularIMC(CalculoIMC paramsCalculoIMC)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Testes com o alias '{nameof(CalculoIMC)}'...");
+        var imc = Math.Round(
+            paramsCalculoIMC.Peso / (paramsCalculoIMC.Altura * paramsCalculoIMC.Altura), 1);
+        string classificacao;
+        if (imc <= 18.5)
+            classificacao = "Magreza";
+        else if (imc <= 25.0)
+            classificacao = "Normal";
+        else if (imc <= 29.9)
+            classificacao = "Sobrepeso";
+        else if (imc <= 40.0)
+            classificacao = "Obesidade";
+        else
+            classificacao = "Obesidade Grave";
+        Console.WriteLine($"Altura = {paramsCalculoIMC.Altura} | " +
+            $"Peso = {paramsCalculoIMC.Peso} | " +
+            $"IMC = {imc} | " +
+            $"Classificacao = {classificacao}");
+    }
+}
