17230934a7bcd01e607a19aada4e31eaa676480e
Renato Groffe <renato.groffe@yahoo.com.br>
2023-11-15 21:55:58 -0300

    Primary Constructors
    Primary Constructors

---
+++ ConsoleAppPrimaryConstructorClass/Models/Projeto.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleAppPrimaryConstructorClass.Models;
+
+public class Projeto(string targetFramework, string descricao, List<string> tecnologias)
+{
+    public Projeto(string targetFramework, string descricao) :
+        this(targetFramework, descricao, new List<string>()) { }
+
+    public string TargetFramework => targetFramework;
+    public string Descricao => descricao;
+    public List<string> Tecnologias => tecnologias;
+    public double HorasTrabalhadas { get; set; }
+}
---
+++ ConsoleAppPrimaryConstructorClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+using ConsoleAppPrimaryConstructorClass.Models;
+
+Console.WriteLine("***** Testes com C# 12 | Primary Constructor em uma classe *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName}- Kernel: {Environment
+    .OSVersion.VersionString}");
+
+Console.WriteLine();
+
+var projeto01 = new Projeto("net7.0", "Worker Consolidacao Bancaria",
+    new List<string>() { "Azure Functions", "Application Storage", "Azure Service Bus" });
+projeto01.Tecnologias.Add("Application Insights");
+projeto01.HorasTrabalhadas = 130.5;
+Console.WriteLine($"{nameof(projeto01)}: {JsonSerializer.Serialize(projeto01)}");
+
+Console.WriteLine();
+
+var projeto02 = new Projeto("net8.0", "API de exemplo demonstrando novos recursos");
+Console.WriteLine($"{nameof(projeto02)}: {JsonSerializer.Serialize(projeto02)}");
