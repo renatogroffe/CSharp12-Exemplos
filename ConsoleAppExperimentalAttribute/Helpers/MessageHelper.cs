1cde839112eff4854e418a2f32196307a98c1706
Renato Groffe <renato.groffe@yahoo.com.br>
2023-11-16 21:04:24 -0300

    ExperimentalAttribute
    ExperimentalAttribute

---
+++ ConsoleAppExperimentalAttribute/Helpers/MessageHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleAppExperimentalAttribute.Helpers;
+
+public static class MessageHelper
+{
+    [Experimental(diagnosticId: "MessageHelper01")]
+    public static void ShowCurrentDateTime()
+    {
+        Console.WriteLine($"Executando o metodo experimental " +
+            $"{nameof(MessageHelper)}.{nameof(ShowCurrentDateTime)}");
+        var now = DateTime.UtcNow.AddHours(-3);
+        Console.WriteLine($"Data: {now:dd} de {now:MMMM} de {now:yyyy}");
+        Console.WriteLine($"Horario atual: {now:HH:mm:ss}");
+    }
+}
---
+++ ConsoleAppExperimentalAttribute/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleAppExperimentalAttribute.Helpers;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("***** Testes com C# 12 | ExperimentalAttribute *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName}- Kernel: {Environment
+    .OSVersion.VersionString}");
+Console.WriteLine();
+
+#pragma warning disable MessageHelper01 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+MessageHelper.ShowCurrentDateTime();
+#pragma warning restore MessageHelper01 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
