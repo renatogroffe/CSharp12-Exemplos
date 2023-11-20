62290d9b0c71d7bc6e4688adf15d94cbba86eb85
Renato Groffe <renato.groffe@yahoo.com.br>
2023-11-19 21:23:00 -0300

    C# 12 + Console Application + .NET 8 + ref readonly parameters
    C# 12 + Console Application + .NET 8 + ref readonly parameters

---
+++ ConsoleAppRefReadOnly/Fatorial.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleAppRefReadOnly;
+
+public class Fatorial
+{
+    public static int Calcular(ref readonly int numero)
+    {
+        int resultado = 1;
+        for (int i = numero; i > 1; i--)
+            resultado *= i;
+
+        return resultado;
+    }
+}
---
+++ ConsoleAppRefReadOnly/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleAppRefReadOnly;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("***** Testes com C# 12 | ref readonly parameters *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName}- Kernel: {Environment
+    .OSVersion.VersionString}");
+Console.WriteLine();
+
+int numero = 0;
+Console.WriteLine($"{numero}! = {Fatorial.Calcular(ref numero)}");
+
+numero = 2;
+Console.WriteLine($"{numero}! = {Fatorial.Calcular(in numero)}");
+
+numero = 4;
+Console.WriteLine($"{numero}! = {Fatorial.Calcular(ref numero)}");
